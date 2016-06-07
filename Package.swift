55e6781769f45dd593135727fa7aa13d33a16286
Muhammad Rusdi <muhammadrusdi@icloud.com>
2016-06-07 09:58:45 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "FluentMongo",
     dependencies: [
-    	.Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 3),
-    	.Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 1, minor: 1)
+    	.Package(url: "https://github.com/qutheory/fluent.git", majorVersion: 0, minor: 3)
     ]
 )
